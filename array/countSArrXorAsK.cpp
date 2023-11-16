a37d9d0fa9a00ec254e6d4e03ae47f0e596fa5b5
harshitsharma25 <omsharma2508@gmail.com>
2023-11-16 18:23:30 +0530

    modify previous file

--- array/countSArrXorAsK.cpp
+++ array/countSArrXorAsK.cpp
@@ -1,20 +1,44 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int countSubarrayWithSumXorK(int arr[],int size,int k){
-    int totalCount = 0;
 
-    for (int i = 0; i < size; i++)
-    {
-        int operation = 0, count = 0;
-       for (int j = i; j < size; j++)
-       {
-          operation ^= arr[j];
-          if(operation == k)  count+=1;
-       }
-       totalCount += count;
+
+                                                                   // BRUTE FORCE T.C = O(N^2)
+// int countSubarrayWithSumXorK(int arr[],int size,int k){
+//     int totalCount = 0;
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         int operation = 0, count = 0;
+//        for (int j = i; j < size; j++)
+//        {
+//           operation ^= arr[j];
+//           if(operation == k)  count+=1;
+//        }
+//        totalCount += count;
        
+//     }
+//     return totalCount;
+// }
+
+
+                                                               // best approach  T.C = O(N)
+
+int countSubarrayWithSumXorK(int arr[],int size, int k){
+    unordered_map<int,int> mp;
+    mp[0] = 1;
+    int opr = 0, count = 0;
+
+    for(int i=0;i<size;i++){
+        opr ^= arr[i];
+        int need = opr ^ k;
+        // if(mp.find(need) != mp.end()){
+            count+=mp[need];
+        
+
+        mp[opr]++;
     }
-    return totalCount;
+    return count;
 }
 
 int main(){
